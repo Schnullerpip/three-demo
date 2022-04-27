576c200f7bec312774d1b8411ff12000e94595ec
julian <mueller@lotum.de>
2022-04-27 14:36:49 +0200

    add scene + geometry

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
+
+
+<!-- SCRIPT -->
 <script setup lang="ts">
-import { getRenderer } from '../src/graphics/three-setup'
+import { getCamera, getRenderer } from '../src/graphics/three-setup'
 import { onMounted } from 'vue'
+import * as THREE from 'three'
+
 
 onMounted(() => {
   const renderer = getRenderer('#c')
+  const camera = getCamera()
+  camera.position.z = 2 // +z goes towards the viewer -z away from viewer
+
+  const scene = new THREE.Scene()
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({color: 0x44aa88})
+  const cube = new THREE.Mesh(geometry, material)
+
+  scene.add(cube)
+
+  function render(time: number) {
+    time *= 0.001
+    cube.rotation.x = time
+    cube.rotation.y = time
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
+  }
+  requestAnimationFrame(render)
 })
 </script>
 
+
+<!-- VUE/HTML TEMPLATE -->
 <template>
   <canvas id="c"></canvas>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<!-- STYLE -->
+<style scoped>
+canvas {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/graphics/three-setup.ts
+++ src/graphics/three-setup.ts
@@ -1,5 +1,8 @@
 import * as THREE from 'three'
 
+/**
+ * Creates a renderer for an existing canvas element with the given id
+ */
 export function getRenderer(canvasId: String): THREE.WebGLRenderer {
     const queriedElement = document.querySelector('#c')
     if(!queriedElement) {
@@ -7,4 +10,16 @@ export function getRenderer(canvasId: String): THREE.WebGLRenderer {
     }
     const canvas = queriedElement as HTMLCanvasElement
     return new THREE.WebGLRenderer({canvas})
+}
+
+/**
+ * Creates a perspective camera
+ */
+export function getCamera(): THREE.PerspectiveCamera {
+    const fov = 75 // vertical field of view
+    const aspect = 2
+    const near = 0.1
+    const far = 5
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+    return camera
 }
